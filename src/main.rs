2692e78113bbb479cd8ae47040610d482895a699
Thaddee Tyl <thaddee.tyl@gmail.com>
2022-01-16 17:13:37 +0100

    Parallelize guess ranking computation
    This allows us to get the root guess rankings within 2.7h instead of 7h.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::fs;
 use std::io::{self, Write};
-use std::time::Instant;
+use std::time::Duration;
+use std::thread;
+use std::sync::{Mutex, Arc};
+use rayon::prelude::*;
 
 enum ConstraintClass { Absent, Misplaced, Correct }
 
@@ -13,13 +16,13 @@ struct Constraint {
 fn constraints(guess: &str, solution: &str) -> Vec<Constraint> {
     let mut constraints = Vec::new();
     for (i, (guess_letter, solution_letter)) in guess.chars().zip(solution.chars()).enumerate() {
-        if guess_letter == solution_letter {
-            constraints.push(Constraint { class: ConstraintClass::Correct, letter: guess_letter, index: i });
+        constraints.push(if guess_letter == solution_letter {
+            Constraint { class: ConstraintClass::Correct, letter: guess_letter, index: i }
         } else if solution.contains(guess_letter) {
-            constraints.push(Constraint { class: ConstraintClass::Misplaced, letter: guess_letter, index: i });
+            Constraint { class: ConstraintClass::Misplaced, letter: guess_letter, index: i }
         } else {
-            constraints.push(Constraint { class: ConstraintClass::Absent, letter: guess_letter, index: i });
-        }
+            Constraint { class: ConstraintClass::Absent, letter: guess_letter, index: i }
+        });
     }
     return constraints;
 }
@@ -27,11 +30,11 @@ fn constraints(guess: &str, solution: &str) -> Vec<Constraint> {
 fn parse_constraints(template: &str, guess: &str) -> Vec<Constraint> {
     let mut constraints = Vec::new();
     for (i, (tc, gc)) in template.chars().zip(guess.chars()).enumerate() {
-        match tc {
-            'o' => constraints.push(Constraint { letter: gc, index: i, class: ConstraintClass::Correct }),
-            'x' => constraints.push(Constraint { letter: gc, index: i, class: ConstraintClass::Misplaced }),
-            _   => constraints.push(Constraint { letter: gc, index: i, class: ConstraintClass::Absent }),
-        }
+        constraints.push(match tc {
+            'o' => Constraint { letter: gc, index: i, class: ConstraintClass::Correct },
+            'x' => Constraint { letter: gc, index: i, class: ConstraintClass::Misplaced },
+            _   => Constraint { letter: gc, index: i, class: ConstraintClass::Absent },
+        });
     }
     return constraints
 }
@@ -53,7 +56,7 @@ fn match_constraints(word: &str, constraints: &Vec<Constraint>) -> bool {
     return true;
 }
 
-fn remaining(guess: &str, solution: &str, solutions: &Vec<&str>) -> f64 {
+fn remaining(guess: &str, solution: &str, solutions: &Vec<String>) -> f64 {
     let constraints = constraints(guess, solution);
     let mut remaining = 0.0;
     for potential_solution in solutions {
@@ -64,7 +67,7 @@ fn remaining(guess: &str, solution: &str, solutions: &Vec<&str>) -> f64 {
     return remaining;
 }
 
-fn avg_remaining(guess: &str, solutions: &Vec<&str>) -> f64 {
+fn avg_remaining(guess: &str, solutions: &Vec<String>) -> f64 {
     let mut remaining_sols = 0.0;
     for solution in solutions {
         remaining_sols += remaining(guess, solution, &solutions);
@@ -77,19 +80,41 @@ struct Choice {
     avg_remaining: f64,
 }
 
+const ANSI_RESET_LINE: &str = "\x1b[1K\x1b[G";
+
 fn rank_guesses(guesses: &Vec<&str>, solutions: &Vec<&str>) -> Vec<Choice> {
-    let mut choices = Vec::new();
-    let mut avg_time = 0.0;
-    for (i, guess) in guesses.iter().enumerate() {
-        print!("\x1b[1K\x1b[GRanking guesses... {}/{} words ({} min left)",
-            i, guesses.len(), (avg_time * (guesses.len()-i) as f64 / 60.0).ceil());
+    let computed_guesses = Arc::new(Mutex::new(0));
+    let computed_guesses_worker = Arc::clone(&computed_guesses);
+    let guesses_worker = guesses.iter().map(|w| w.to_string()).collect::<Vec<String>>();
+    let solutions_worker = solutions.iter().map(|w| w.to_string()).collect::<Vec<String>>();
+    let workers: thread::JoinHandle<Vec<Choice>> = thread::spawn(move ||
+        guesses_worker.par_iter().map(|guess| {
+            let choice = Choice { word: guess.to_string(), avg_remaining: avg_remaining(guess, &solutions_worker) };
+            let mut counter = computed_guesses_worker.lock().unwrap();
+            *counter += 1;
+            return choice;
+        }).collect()
+    );
+
+    print!("Ranking guesses...");
+    let mut guesses_per_sec = 0.0;
+    let mut prev_counter = 0;
+    let mut i = 0.0;
+    loop {
+        thread::sleep(Duration::from_secs(1));
+        let counter = computed_guesses.lock().unwrap();
+        if *counter >= guesses.len() { break; }
+        print!("{}Ranking guesses... {}/{} words ({} min left)",
+            ANSI_RESET_LINE, *counter, guesses.len(), ((guesses.len()-*counter) as f64 / guesses_per_sec / 60.0).ceil());
         io::stdout().flush().unwrap();
-        let time = Instant::now();
-        choices.push(Choice { word: guess.to_string(), avg_remaining: avg_remaining(guess, &solutions) });
-        avg_time = (i as f64 * avg_time + time.elapsed().as_secs_f64()) / (i+1) as f64;
+        guesses_per_sec = (i * guesses_per_sec + (*counter - prev_counter) as f64) / (i + 1.0);
+        prev_counter = *counter;
+        i += 1.0;
     }
-    print!("\x1b[1K\x1b[G");
-    choices.sort_by(|a, b| a.avg_remaining.partial_cmp(&b.avg_remaining).unwrap());
+    print!("{}", ANSI_RESET_LINE);
+
+    let mut choices = workers.join().unwrap();
+    choices.sort_by(|a, b| (*a).avg_remaining.partial_cmp(&b.avg_remaining).unwrap());
     return choices;
 }
 
@@ -97,8 +122,8 @@ fn main() {
     // Parse valid words.
     let solutions_content = fs::read_to_string("solutions")
         .expect("Failed to read solutions file");
-    let solutions: Vec<&str> = solutions_content.lines().collect();
-    //let solutions: Vec<&str> = solutions_content.lines().take(10).collect();
+    let solutions: Vec<&str> = (&solutions_content).lines().collect();
+    //let solutions: Vec<&str> = solutions_content.lines().take(100).collect();
     let non_solution_guesses_content = fs::read_to_string("non-solution-guesses")
         .expect("Failed to read solutions file");
     let non_solution_guesses: Vec<&str> = non_solution_guesses_content.lines().collect();
@@ -107,7 +132,7 @@ fn main() {
     // Start the guessing game.
     let mut remaining_solutions = solutions.clone();
     while remaining_solutions.len() > 1 {
-        for (i, top_choice) in rank_guesses(&allowed_guesses, &remaining_solutions).iter().enumerate() {
+        for (i, top_choice) in rank_guesses(&allowed_guesses, &remaining_solutions).iter().take(10).enumerate() {
             println!("{}. {} (keeps {} words on average)", i, top_choice.word, top_choice.avg_remaining);
         }
 
