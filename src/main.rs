792c12d9cb1b1eb7bae6ef190386f2a83e469632
Thaddee Tyl <thaddee.tyl@gmail.com>
2022-01-22 14:04:48 +0100

    Preload precomputed root guess ratings
    This makes a typical session guess time actually usable:
    instead of waiting for 3h to start getting suggestions,
    we get the first suggestions instantly,
    and the next one takes about a minute.

--- src/main.rs
+++ src/main.rs
@@ -1,84 +1,12 @@
+mod wordle;
+mod choice;
+use choice::Choice;
 use std::fs;
 use std::io::{self, Write};
 use std::time::Duration;
 use std::thread;
 use std::sync::{Mutex, Arc};
 use rayon::prelude::*;
-
-enum ConstraintClass { Absent, Misplaced, Correct }
-
-struct Constraint {
-    class: ConstraintClass,
-    letter: char,
-    index: usize,
-}
-
-fn constraints(guess: &str, solution: &str) -> Vec<Constraint> {
-    let mut constraints = Vec::new();
-    for (i, (guess_letter, solution_letter)) in guess.chars().zip(solution.chars()).enumerate() {
-        constraints.push(if guess_letter == solution_letter {
-            Constraint { class: ConstraintClass::Correct, letter: guess_letter, index: i }
-        } else if solution.contains(guess_letter) {
-            Constraint { class: ConstraintClass::Misplaced, letter: guess_letter, index: i }
-        } else {
-            Constraint { class: ConstraintClass::Absent, letter: guess_letter, index: i }
-        });
-    }
-    return constraints;
-}
-
-fn parse_constraints(template: &str, guess: &str) -> Vec<Constraint> {
-    let mut constraints = Vec::new();
-    for (i, (tc, gc)) in template.chars().zip(guess.chars()).enumerate() {
-        constraints.push(match tc {
-            'o' => Constraint { letter: gc, index: i, class: ConstraintClass::Correct },
-            'x' => Constraint { letter: gc, index: i, class: ConstraintClass::Misplaced },
-            _   => Constraint { letter: gc, index: i, class: ConstraintClass::Absent },
-        });
-    }
-    return constraints
-}
-
-fn match_constraint(word: &str, constraint: &Constraint) -> bool {
-    match constraint.class {
-        ConstraintClass::Correct => word.chars().nth(constraint.index).unwrap() == constraint.letter,
-        ConstraintClass::Misplaced => word.chars().nth(constraint.index).unwrap() != constraint.letter && word.contains(constraint.letter),
-        ConstraintClass::Absent => !word.contains(constraint.letter),
-    }
-}
-
-fn match_constraints(word: &str, constraints: &Vec<Constraint>) -> bool {
-    for constraint in constraints {
-        if !match_constraint(word, constraint) {
-            return false;
-        }
-    }
-    return true;
-}
-
-fn remaining(guess: &str, solution: &str, solutions: &Vec<String>) -> f64 {
-    let constraints = constraints(guess, solution);
-    let mut remaining = 0.0;
-    for potential_solution in solutions {
-        if match_constraints(potential_solution, &constraints) {
-            remaining += 1.0;
-        }
-    }
-    return remaining;
-}
-
-fn avg_remaining(guess: &str, solutions: &Vec<String>) -> f64 {
-    let mut remaining_sols = 0.0;
-    for solution in solutions {
-        remaining_sols += remaining(guess, solution, &solutions);
-    }
-    return remaining_sols / solutions.len() as f64;
-}
-
-struct Choice {
-    word: String,
-    avg_remaining: f64,
-}
 
 const ANSI_RESET_LINE: &str = "\x1b[1K\x1b[G";
 
@@ -89,7 +17,7 @@ fn rank_guesses(guesses: &Vec<&str>, solutions: &Vec<&str>) -> Vec<Choice> {
     let solutions_worker = solutions.iter().map(|w| w.to_string()).collect::<Vec<String>>();
     let workers: thread::JoinHandle<Vec<Choice>> = thread::spawn(move ||
         guesses_worker.par_iter().map(|guess| {
-            let choice = Choice { word: guess.to_string(), avg_remaining: avg_remaining(guess, &solutions_worker) };
+            let choice = Choice { word: guess.to_string(), avg_remaining: choice::avg_remaining(guess, &solutions_worker) };
             let mut counter = computed_guesses_worker.lock().unwrap();
             *counter += 1;
             return choice;
@@ -132,7 +60,12 @@ fn main() {
     // Start the guessing game.
     let mut remaining_solutions = solutions.clone();
     while remaining_solutions.len() > 1 {
-        for (i, top_choice) in rank_guesses(&allowed_guesses, &remaining_solutions).iter().take(10).enumerate() {
+        let suggestions = if remaining_solutions.len() == solutions.len() {
+            choice::root_choices()
+        } else {
+            rank_guesses(&allowed_guesses, &remaining_solutions)
+        };
+        for (i, top_choice) in suggestions.iter().take(10).enumerate() {
             println!("{}. {} (keeps {} words on average)", i, top_choice.word, top_choice.avg_remaining);
         }
 
@@ -144,9 +77,9 @@ fn main() {
         io::stdin().read_line(&mut constraint_template).ok();
 
         // Filter remaining solutions.
-        let constraints = parse_constraints(&constraint_template, &guess);
+        let constraints = wordle::parse_constraints(&constraint_template, &guess);
         remaining_solutions = remaining_solutions.into_iter().filter(|sol|
-            match_constraints(sol, &constraints)).collect();
+            wordle::match_constraints(sol, &constraints)).collect();
     }
 
     println!("Solution: {}.", remaining_solutions[0]);
---
+++ src/wordle.rs
@@ -0,0 +1,50 @@
+pub enum ConstraintClass { Absent, Misplaced, Correct }
+
+pub struct Constraint {
+    class: ConstraintClass,
+    letter: char,
+    index: usize,
+}
+
+pub fn constraints(guess: &str, solution: &str) -> Vec<Constraint> {
+    let mut constraints = Vec::new();
+    for (i, (guess_letter, solution_letter)) in guess.chars().zip(solution.chars()).enumerate() {
+        constraints.push(if guess_letter == solution_letter {
+            Constraint { class: ConstraintClass::Correct, letter: guess_letter, index: i }
+        } else if solution.contains(guess_letter) {
+            Constraint { class: ConstraintClass::Misplaced, letter: guess_letter, index: i }
+        } else {
+            Constraint { class: ConstraintClass::Absent, letter: guess_letter, index: i }
+        });
+    }
+    return constraints;
+}
+
+pub fn parse_constraints(template: &str, guess: &str) -> Vec<Constraint> {
+    let mut constraints = Vec::new();
+    for (i, (tc, gc)) in template.chars().zip(guess.chars()).enumerate() {
+        constraints.push(match tc {
+            'o' => Constraint { letter: gc, index: i, class: ConstraintClass::Correct },
+            'x' => Constraint { letter: gc, index: i, class: ConstraintClass::Misplaced },
+            _   => Constraint { letter: gc, index: i, class: ConstraintClass::Absent },
+        });
+    }
+    return constraints
+}
+
+pub fn match_constraint(word: &str, constraint: &Constraint) -> bool {
+    match constraint.class {
+        ConstraintClass::Correct => word.chars().nth(constraint.index).unwrap() == constraint.letter,
+        ConstraintClass::Misplaced => word.chars().nth(constraint.index).unwrap() != constraint.letter && word.contains(constraint.letter),
+        ConstraintClass::Absent => !word.contains(constraint.letter),
+    }
+}
+
+pub fn match_constraints(word: &str, constraints: &Vec<Constraint>) -> bool {
+    for constraint in constraints {
+        if !match_constraint(word, constraint) {
+            return false;
+        }
+    }
+    return true;
+}
